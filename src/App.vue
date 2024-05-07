5dce6381070bb60f01ded24e26361d6bd2009f9d
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-05-07 12:32:00 +0200

    Add initial structure for query

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@ import axios from 'axios';
 export default {
     data() {
         return {
-
+            response : '',
+            content : '',
+            value_1: 'EUR',
+            value_2: 'BRL',
         };
     },
     components: {
@@ -15,10 +18,12 @@ export default {
 
     },
     mounted() {
+        const url = 'https://api.frankfurter.app/latest?from=' + this.value_1 + '&to=' + this.value_2;
         axios
-            .get('')
-            .then((response) => {
-                console.log(response)
+            .get(url)
+            .then((res) => {
+                this.response = res.data
+                console.log(this.response)
             });
     }
 }
@@ -27,6 +32,18 @@ export default {
 <template>
 
     <AppMain />
+
+    <h1 class="text-center">Value Converter</h1>
+    
+        <h1>{{ response.base }} - {{ response.amount }}</h1>
+        <ul>
+            <li v-for="(value, key) in response.rates">
+                {{ key }} - {{ value }}
+            </li>
+        </ul>
+
+        <input type ="text" v-model="content" placeholder = "Inserisci qualcosa">
+
 </template>
 
 <style lang="scss">
